4bd43a7ec523846006040a0164a2ed4858386e50
moraismariana <moraismariana200@gmail.com>
2024-08-27 16:21:32 -0300

    Slide Portfolio

---
+++ script.js
@@ -0,0 +1,32 @@
+// SLIDER
+let screenWidth = window.innerWidth;
+const splideOptions = {
+  autoplay: true,
+  arrows: false,
+  drag: false,
+  pauseOnHover: false,
+
+  interval: 5000,
+  speed: 1100,
+  focus: "center",
+  slideFocus: true,
+  type: "loop",
+  fixedWidth: "60vw",
+  fixedHeight: "70vh",
+  gap: "12px",
+
+  breakpoints: {
+    1300: {
+      fixedWidth: "70vw",
+      fixedHeight: "80vh",
+    },
+    768: {
+      fixedWidth: screenWidth - 24,
+      fixedHeight: "500px",
+      drag: true,
+      arrows: false,
+    },
+  },
+};
+
+new Splide(".splide", splideOptions).mount();
