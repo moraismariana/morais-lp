4f3d35f2d1e1476dfc1d8d2322770a527dfb0f40
moraismariana <moraismariana200@gmail.com>
2024-08-29 14:05:24 -0300

    Unidade de medida dvh

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// LOADER
-document.addEventListener("DOMContentLoaded", function () {
-  const loaderContainer = document.querySelector(".loader-container");
-
-  // Aplica a classe 'loading' para exibir o loader
-  document.body.classList.add("loading");
-
-  window.addEventListener("load", function () {
-    // Remove a classe 'loading' e adiciona 'loaded' após o carregamento completo
-    document.body.classList.remove("loading");
-    document.body.classList.add("loaded");
-
-    // Espera o tempo da transição (0.5s) e então oculta o elemento
-    setTimeout(function () {
-      loaderContainer.style.display = "none";
-    }, 500);
-  });
-});
-
 // SLIDER
 let screenWidth = window.innerWidth;
 const splideOptions = {
